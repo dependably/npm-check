bdd556381fbd2f808217530ec21e7f4d9248d642
Michael Hiland <michael.hiland@owasp.org>
2026-01-25 22:54:15 -0800

    update project plan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
-export { parsePackageLock } from './parser.js';
+export { parseLockfile, serializeLockfile } from './parser.js';
 export { validatePackageLock, ValidationError } from './validator.js';
 export { migrateToVersion, PackageLockMigrator, MigrationError } from './migrator.js';
 export { upgradeIntegrityHashes, deduplicatePackages } from './updater.js';
+export { fixPackageLock } from './fixer.js';
 export { LOCKFILE_VERSIONS } from './format-library.js';
